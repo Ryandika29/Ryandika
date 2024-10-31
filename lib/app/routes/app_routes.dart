3132c749d97aa009125afe3053b6a1947b3e321f
riyandikaibrahim0311 <riyandikaibrahim0311@gmail.com>
2024-10-31 00:54:55 +0000

    first commit

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,22 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._(); // Private constructor to prevent instantiation
+  
+  static const HOME = _Paths.HOME;
+  static const LOGIN = _Paths.LOGIN;
+  static const SIGNUP = _Paths.SIGNUP;
+  static const RESET_PASSWORD = _Paths.RESET_PASSWORD;
+}
+
+abstract class _Paths {
+  _Paths._(); // Private constructor to prevent instantiation
+  
+  static const HOME = '/home';
+  static const LOGIN = '/login';
+  static const SIGNUP = '/signup';
+  static const RESET_PASSWORD = '/reset-password';
+}
